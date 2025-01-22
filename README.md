3ff24d0868bb2978815c1ac9b6f62edb4845d553
J-hune <donovann@jhune.dev>
2025-01-22 14:33:40 +0100

    feat: add Polyscope to the project

---
+++ README.md
@@ -0,0 +1,27 @@
+# OrCoeur
+
+### Prerequisites
+- `CMake` (version 3.20 or higher)
+- `C++` compiler with `C++23` support
+
+
+### Compilation Instructions
+1. Clone the repository
+    ```bash
+    git clone https:://github.com/____/____.git
+    cd OrCoeur
+    git submodule update --init --recursive
+    ```
+
+2. Create a build directory and compile the project:
+    ```bash
+    mkdir build
+    cd build
+    cmake ..
+    make
+    ```
+
+3. Run the project:
+    ```bash
+    ./OrCoeur
+    ```
--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
-#include <iostream>
+#include "polyscope/polyscope.h"
+#include "polyscope/surface_mesh.h"
 
-int main() {
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+int main(int argc, char** argv) {
+    // Initialise Polyscope
+    polyscope::init();
 
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
-    }
+    // Create a simple tetrahedron
+    std::vector<std::array<double, 3>> vertices = {
+        {0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}
+    };
+    std::vector<std::array<int, 3>> faces = {
+        {0, 1, 2}, {0, 1, 3}, {0, 2, 3}, {1, 2, 3}
+    };
+
+    // Register the mesh with Polyscope
+    polyscope::registerSurfaceMesh("Simple Tetrahedron", vertices, faces);
+
+    // Show the Polyscope GUI
+    polyscope::show();
 
     return 0;
 }
